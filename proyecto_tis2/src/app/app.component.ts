ac809da0eab9f0d0d3a5a9edab3ba4a994dae641
mcerda98 <mcerda@ing.ucsc.cl>
2021-05-25 14:12:29 -0400

    Menu lateral
    aún falta que los tabs no desaparezcan después de elegir la pagina home en el menú lateral, pero después de que recarga la pagina, se muestra el menu con los tabs

--- proyecto_tis2/src/app/app.component.ts
+++ proyecto_tis2/src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = TabsPage;
+  pages: Array<{title: string, component: any}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -18,5 +21,13 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    this.pages = [
+      { title: 'Home', component: HomePage }
+    ];
+  }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
