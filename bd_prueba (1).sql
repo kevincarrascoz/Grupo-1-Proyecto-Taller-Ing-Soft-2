247c27bc6eff02eba2b965be88d7a88a46e5258c
sessionmaestra <70710866+sessionmaestra@users.noreply.github.com>
2021-05-27 20:25:28 -0400

    bd-prueba

--- bd_prueba (1).sql
+++ bd_prueba (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-05-2021 a las 01:24:53
+-- Tiempo de generación: 28-05-2021 a las 02:24:50
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
