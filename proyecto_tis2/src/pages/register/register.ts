3dc72ffbf40f7c179b1d6607d2299eaa0562d5c9
Constanza Garcés Escobar <41880553+Coniconstanza@users.noreply.github.com>
2021-05-31 17:45:52 -0400

    detalles

--- proyecto_tis2/src/pages/register/register.ts
+++ proyecto_tis2/src/pages/register/register.ts
@@ -1,9 +1,6 @@
 import { Component} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-
-
-
 /**
  * Generated class for the RegisterPage page.
  *
@@ -17,13 +14,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-  userData = {"nombre":"", "apellido":"","correo":"", "contrasena":"","direccion":"","fecha_nacimiento":"","telefono":""};
+  nombre:string;
+  apellido:string;
+  correo:string;
+  contrasena:string;
+  direccion:string;
+  fecha_nacimiento:Date;
+  telefono:string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
 
   Registro(){
-
+    if(this.nombre==undefined || this.apellido==undefined || this.correo==undefined || this.contrasena==undefined 
+      || this.direccion==undefined || this.fecha_nacimiento==undefined || this.telefono==undefined ){
+      alert("Ingrese todos los datos");
+    }else{
+    
+  }
   }
 
   IrLogin(){
