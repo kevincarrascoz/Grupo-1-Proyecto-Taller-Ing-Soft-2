429f6b79aad29e426c40a9247ebe02906899f0fe
Constanza Garcés Escobar <41880553+Coniconstanza@users.noreply.github.com>
2021-06-14 22:56:31 -0400

    delete tabs

--- proyecto_tis2/src/app/app.module.ts
+++ proyecto_tis2/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { SearchBarPage } from '../pages/search-bar/search-bar';
 import { HistorialPage } from '../pages/historial/historial';
 import { PublicarPage } from '../pages/publicar/publicar';
@@ -39,7 +38,6 @@ import { PublicacionesPage } from '../pages/publicaciones/publicaciones';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
     SearchBarPage,
     HistorialPage,
     PublicarPage,
@@ -70,7 +68,6 @@ import { PublicacionesPage } from '../pages/publicaciones/publicaciones';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
     SearchBarPage,
     HistorialPage,
     PublicarPage,
