ba7f39b877ead173075a756f5597443a5751f00e
mcerda98 <70710519+mcerda98@users.noreply.github.com>
2021-06-29 22:04:59 -0400

    publicaciones mas antiguas y actuales
    PHP's para obtener datos de las publicaciones con las consultas correspondientes

---
+++ proyecto_tis2/ordenar_mas_actual.php
@@ -0,0 +1,41 @@
+<?php
+include "config.php";
+include "utils.php";
+
+
+$dbConn =  connect($db);
+
+/*
+  listar todos los posts o solo uno
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id_publicacion']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * FROM publicacion, usuario, oficio 
+                               where id_publicacion=:id_publicacion 
+                               and usuario.correo=publicacion.correo 
+                               and oficio.id_oficio=publicacion.id_oficio
+                               order by publicacion.fecha_publicacion desc");
+      $sql->bindValue(':id_publicacion', $_GET['id_publicacion']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+    else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM publicacion,usuario,oficio 
+                               where usuario.correo=publicacion.correo 
+                               and oficio.id_oficio=publicacion.id_oficio
+                               order by publicacion.fecha_publicacion desc");
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
+}
+
+
+?>
