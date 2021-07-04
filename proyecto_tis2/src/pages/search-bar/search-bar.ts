27d0ce9c4f3048ae3b5a0933a9aae6101e938b78
mcerda98 <70710519+mcerda98@users.noreply.github.com>
2021-07-03 22:57:10 -0400

    buscador (aun no completado)

--- proyecto_tis2/src/pages/search-bar/search-bar.ts
+++ proyecto_tis2/src/pages/search-bar/search-bar.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { DetallepublicacionPage } from '../detallepublicacion/detallepublicacion';
 
 
 /**
@@ -16,16 +17,64 @@ import { Http } from '@angular/http';
   templateUrl: 'search-bar.html',
 })
 export class SearchBarPage {
+  publicaciones:any;
+  id:any;
+  correo: any;
+  contrasena: any;
+  oficios: any;
+  oficio_name: any;
   public isSearchbarOpened = true;
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
-     
+    this.correo = navParams.get('correo');
+    this.contrasena = navParams.get('contrasena');
+    console.log(this.correo,this.contrasena);
+    this.http.get('http://localhost/xampp/Grupo-1-Proyecto-Taller-Ing-Soft-2/proyecto_tis2/publicaciones.php/')
+    //this.http.get('https://proyectooficiosapp.000webhostapp.com/publicaciones.php')
+    .map(response => response.json())
+    .subscribe(data =>
+      {
+        this.publicaciones = data;
+        console.log(this.publicaciones);
+
+        for (let i = 0; i < data; i++) {
+        this.publicaciones.push( this.publicaciones.length );
+      }
+        
+      },
+      err =>{
+        console.log("Oops!");
+        //this.presentToast("No existen registros aun");
+      }
+      );
   }
   onSearch(event){
     console.log(event.target.value);
+    const val = String(event.target.value);
+    if(val && val.trim() != ''){
+      this.publicaciones = this.publicaciones.filter(ofi => {
+        return (ofi.nombre_oficio.toLowerCase().indexOf(val.toLowerCase())>-1);
+      })
+    }
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchBarPage'); 
     
   }
- 
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < this.publicaciones; i++) {
+        this.publicaciones.push( this.publicaciones.length );
+      }
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+  detalle(id){
+    this.navCtrl.push(DetallepublicacionPage,{valor:id})
+  }
 }
