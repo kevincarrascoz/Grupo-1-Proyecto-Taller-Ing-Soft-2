7314d0fa930eec76eefb60ce37fe1d277c0866f0
mcerda98 <70710519+mcerda98@users.noreply.github.com>
2021-07-05 19:10:02 -0400

    Update pub-oficio.ts

--- proyecto_tis2/src/pages/pub-oficio/pub-oficio.ts
+++ proyecto_tis2/src/pages/pub-oficio/pub-oficio.ts
@@ -24,8 +24,14 @@ import { SearchBarPage } from '../search-bar/search-bar';
 export class PubOficioPage {
   id_oficio = this.navParams.get('valor');
   publicaciones: any = [];
+  data2:any;
+  correo:any;
+  contrasena: any;
+  correo1: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
-    console.log(this.id_oficio);
+    this.correo = navParams.get('correo');
+    this.contrasena = navParams.get('contrasena');
+    console.log(this.correo,this.contrasena);
     this.http.get("http://localhost/xampp/Grupo-1-Proyecto-Taller-Ing-Soft-2/proyecto_tis2/pub_oficio.php?id_oficio="+this.id_oficio)
     .map(response => response.json())
     .subscribe(data => {
@@ -57,7 +63,38 @@ export class PubOficioPage {
     }, 500);
   }
   detalle(id){
-    this.navCtrl.push(DetallepublicacionPage, {valor: id});
+    if(this.correo1==this.correo){
+      this.navCtrl.push(DetallepublicacionLogoutPage,{valor:id});
+    }else{
+      var headers = new Headers();
+      headers.append("Accept", 'application/json');
+      headers.append('Content-Type', 'application/json' );
+      let options = new RequestOptions({ headers: headers });
+       let data2 = {
+        correo: this.correo,
+        id_publicacion: id,
+      };
+      console.log(data2);
+      this.http.post('http://localhost/xampp/Grupo-1-Proyecto-Taller-Ing-Soft-2/proyecto_tis2/historial.php',data2, options)
+        //this.http.post('https://proyectooficiosapp.000webhostapp.com/register.php',data, options)
+        .map(res => res.json())
+        .subscribe(res => {
+        
+       
+        if(res=="Historial exitoso"){
+        
+            console.log('registro exitoso');
+          }
+        
+        else
+        {
+        console.log('error');
+          } 
+        });
+        
+      this.navCtrl.push(DetallepublicacionPage,{valor:id, correo: this.correo});
+    }
+    
   }
   buscar(){
     this.navCtrl.push(SearchBarPage);
