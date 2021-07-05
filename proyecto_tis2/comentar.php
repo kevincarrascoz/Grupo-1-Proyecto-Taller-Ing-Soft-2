8c458167c012e63b9d30f83948e040f0e64fcf2f
Constanza Garcés Escobar <41880553+Coniconstanza@users.noreply.github.com>
2021-07-05 00:01:55 -0400

    agrega comentarios, solo falta mostrarlos

---
+++ proyecto_tis2/comentar.php
@@ -0,0 +1,51 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        header('Content-Type:application/json');
+    }
+ 
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+ 
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            header('Content-Type:application/json');         
+ 
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            header('Content-Type:application/json');
+ 
+        exit(0);
+    }
+ 
+  require "dbconnect.php";
+  
+    $data = file_get_contents("php://input");
+    if (isset($data)) {
+        $request = json_decode($data);
+		$comentario = $request->comentario;
+        $correo = $request->correo;
+	}
+
+    date_default_timezone_set('America/Santiago');
+    $fecha_actual = date("Y-m-d H:i:s");
+
+$sql = "INSERT INTO comentario (id_comentario, comentario, fecha_comentario,correo)
+VALUES ('', '$comentario', '$fecha_actual', '$correo')";
+
+
+if ($con->query($sql) === TRUE) {
+	$response= "Comentario exitoso";
+   
+} else {
+   $response= "Error: " . $sql . "<br>" . $db->error;
+}
+ 
+  
+	echo json_encode( $response);
+
+ 
+?>
