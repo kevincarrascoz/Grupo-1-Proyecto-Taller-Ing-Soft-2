7129ce94d1cfdaffbd02473b729621ee01f6a5fb
Constanza Garcés Escobar <41880553+Coniconstanza@users.noreply.github.com>
2021-07-22 00:17:00 -0400

    delete pags innecesarias

--- proyecto_tis2/src/app/app.module.ts
+++ proyecto_tis2/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
 import { SearchBarPage } from '../pages/search-bar/search-bar';
 import { HistorialPage } from '../pages/historial/historial';
 import { PublicarPage } from '../pages/publicar/publicar';
@@ -35,7 +34,6 @@ import { FavoritosPage } from '../pages/favoritos/favoritos';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     SearchBarPage,
     HistorialPage,
     PublicarPage,
@@ -65,7 +63,6 @@ import { FavoritosPage } from '../pages/favoritos/favoritos';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
     SearchBarPage,
     HistorialPage,
     PublicarPage,
