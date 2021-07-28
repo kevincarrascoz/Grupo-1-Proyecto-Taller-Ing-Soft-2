e6807258a0c5f81d454133134c5539e914e011df
kevincarrascoz <71083755+kevincarrascoz@users.noreply.github.com>
2021-07-28 16:45:01 -0400

    Update register.ts

--- proyecto_tis2/src/pages/register/register.ts
+++ proyecto_tis2/src/pages/register/register.ts
@@ -26,7 +26,6 @@ export class RegisterPage {
   @ViewChild("fecha_nacimiento") fecha_nacimiento;
   @ViewChild("telefono") telefono;
   @ViewChild("comuna") comuna;
-  @ViewChild("tipo_usuario") tipo_usuario;
   comunas:any;
   
   
@@ -99,12 +98,6 @@ export class RegisterPage {
         duration: 3000
       });
       toast.present();
-    }else if(this.tipo_usuario.value==""){
-      const toast = this.toastCtrl.create({
-        message: 'Ingrese un tipo de usuario', 
-        duration: 3000
-      });
-      toast.present();
     }else{
 
       var headers = new Headers();
@@ -123,7 +116,6 @@ export class RegisterPage {
         fecha_nacimiento: this.fecha_nacimiento.value,
         telefono: this.telefono.value,
         comuna: this.comuna.value,
-        tipo_usuario: this.tipo_usuario.value
       };
       console.log(data);
       let loader = this.loading.create({
